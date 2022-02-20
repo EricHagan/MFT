0e4439382a026821d171f2638265f36ceb57db52
Matt Garrett <matt.garrett@crtech.com>
2022-02-19 18:03:38 -0700

    add DesignTimeHelper class to determine when controls, etc. are loaded within the VS Designer

---
+++ MFT/DesignTimeHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace MFT
+{
+    /// <summary>
+    /// Use to check if the process is being run inside the Visual Studio Designer
+    /// From https://stackoverflow.com/questions/2427381/how-to-detect-that-c-sharp-windows-forms-code-is-executed-within-visual-studio
+    /// </summary>
+    internal static class DesignTimeHelper
+    {
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                bool isInDesignMode = LicenseManager.UsageMode == LicenseUsageMode.Designtime || Debugger.IsAttached == true;
+
+                if (!isInDesignMode)
+                {
+                    using (var process = Process.GetCurrentProcess())
+                    {
+                        return process.ProcessName.ToLowerInvariant().Contains("devenv");
+                    }
+                }
+
+                return isInDesignMode;
+            }
+        }
+    }
+}
