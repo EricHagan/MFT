1f216ac2cda7c4e7be639d926552dbf7d370edf7
Matt Garrett <matt.garrett@crtech.com>
2022-02-20 21:22:57 -0700

    add Workspace and SpectrumProcessorChain classes

---
+++ MFT/SpectrumProcessorChain.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MFT
+{
+    internal class SpectrumProcessorChain : IEnumerable<ISpectrumProcessor>, IList<ISpectrumProcessor>, ISpectrumProcessor
+    {
+        public Spectrum Process(Spectrum data)
+        {
+            if (chain == null)
+                return data;
+            if (chain.Count == 0)
+                return data;
+            Spectrum spectrum = data;
+            foreach(ISpectrumProcessor processor in chain)
+            {
+                spectrum = processor.Process(spectrum);
+            }
+            return spectrum;
+        }
+
+        public int ID { get; set; }
+
+        List<ISpectrumProcessor> chain => new List<ISpectrumProcessor>();
+
+        #region IEnumerable
+
+        public IEnumerator<ISpectrumProcessor> GetEnumerator()
+        {
+            return ((IEnumerable<ISpectrumProcessor>)chain).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)chain).GetEnumerator();
+        }
+
+        #endregion
+
+        #region IList
+
+        public int Count => ((ICollection<ISpectrumProcessor>)chain).Count;
+
+        public bool IsReadOnly => ((ICollection<ISpectrumProcessor>)chain).IsReadOnly;
+
+        public ISpectrumProcessor this[int index] { get => ((IList<ISpectrumProcessor>)chain)[index]; set => ((IList<ISpectrumProcessor>)chain)[index] = value; }
+
+        public int IndexOf(ISpectrumProcessor item)
+        {
+            return ((IList<ISpectrumProcessor>)chain).IndexOf(item);
+        }
+
+        public void Insert(int index, ISpectrumProcessor item)
+        {
+            ((IList<ISpectrumProcessor>)chain).Insert(index, item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            ((IList<ISpectrumProcessor>)chain).RemoveAt(index);
+        }
+
+        public void Add(ISpectrumProcessor item)
+        {
+            ((ICollection<ISpectrumProcessor>)chain).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((ICollection<ISpectrumProcessor>)chain).Clear();
+        }
+
+        public bool Contains(ISpectrumProcessor item)
+        {
+            return ((ICollection<ISpectrumProcessor>)chain).Contains(item);
+        }
+
+        public void CopyTo(ISpectrumProcessor[] array, int arrayIndex)
+        {
+            ((ICollection<ISpectrumProcessor>)chain).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(ISpectrumProcessor item)
+        {
+            return ((ICollection<ISpectrumProcessor>)chain).Remove(item);
+        }
+
+        #endregion
+    }
+}
---
+++ MFT/Workspace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFT
+{
+    public class Workspace
+    {
+        ISpectrometer spectrometer { get; set; }
+        Dictionary<int, SpectrumProcessorChain> SpectrumProcessorChains { get; set; }
+        // todo: Dictionary<int, Test>
+    }
+}
