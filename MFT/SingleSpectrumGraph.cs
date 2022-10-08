ba9e9fdc679fa565c7aadb60a6cf1b13abd37b4e
Matt Garrett <matt.garrett@crtech.com>
2022-10-08 11:26:59 -0400

    get rid of unused secondary controls

--- MFT/SingleSpectrumGraph.cs
+++ MFT/SingleSpectrumGraph.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 
@@ -39,11 +32,6 @@ namespace MFT
             }
         }
         Exposure exposure;
-
-        public void ExposureResampledHandler(object sender, ExposureResampledEventArgs e)
-        {
-            Exposure = e.ResampledExposure;
-        }
 
         public void UpdateValues()
         {
@@ -94,7 +82,5 @@ namespace MFT
             chart1.ChartAreas[0].AxisY.Maximum = yMax;
             chart1.ChartAreas[0].AxisY.Interval = interval;
         }
-
-
     }
 }
