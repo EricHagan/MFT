00542436f99d18968d6c7e8c24a894823373d446
Matt Garrett <matt.garrett@crtech.com>
2022-10-09 22:16:34 -0400

    add Message and Messenger classes

---
+++ MFT/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MFT
+{
+    public class Message : EventArgs
+    {
+        public Message(Types type, Object _object)
+        {
+            Type = type;
+            Object = _object;
+        }
+
+        public Types Type { get; private set; }
+        public Object Object { get; private set; }
+
+        public enum Types
+        {
+            EXPOSURE_SETTINGS_CREATE,
+            EXPOSURE_SETTINGS_UPDATED,
+            EXPOSURE_SETTINGS_SET_DEFAULT
+        }
+    }
+}
---
+++ MFT/Messenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MFT
+{
+    public static class Messenger
+    {
+        public static void SendMessage(object sender, Message msg)
+        {
+            MessageAvailable?.Invoke(sender, msg);
+        }
+
+        public static event EventHandler<Message> MessageAvailable;
+    }
+}
