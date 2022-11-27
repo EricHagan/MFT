1cf81a8bfa674097ff04e19c88c378f65eeb4b01
Matt Garrett <matt.garrett@crtech.com>
2022-11-27 11:00:28 -0500

    add Messenger.activeEvents counter

--- MFT/Messenger.cs
+++ MFT/Messenger.cs
@@ -6,9 +6,13 @@ namespace MFT
     {
         public static void SendMessage(object sender, Message.Types type, object _object)
         {
+            activeEvents++;
             MessageAvailable?.Invoke(sender, new Message(type, _object));
+            activeEvents--;
         }
 
         public static event EventHandler<Message> MessageAvailable;
+
+        static int activeEvents = 0;
     }
 }
