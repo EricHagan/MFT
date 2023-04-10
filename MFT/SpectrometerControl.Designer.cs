0aedb15193c460b48d7cdb09f251f697e92bc752
Matt Garrett <matt.garrett@ansys.com>
2023-04-09 20:21:42 -0600

    add spectrum processor chain control stub to spectrometer control

--- MFT/SpectrometerControl.Designer.cs
+++ MFT/SpectrometerControl.Designer.cs
@@ -41,23 +41,28 @@
             this.label3 = new System.Windows.Forms.Label();
             this.averagingNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.spectrumProcessorChainControl1 = new MFT.SpectrumProcessorChainControl();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dwellTimeNumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.integrationTimeMsNumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.averagingNumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
             this.splitContainer1.Panel2.Controls.Add(this.saveExposureSettingsButton);
             this.splitContainer1.Panel2.Controls.Add(this.whiteRefButton);
             this.splitContainer1.Panel2.Controls.Add(this.darkRefButton);
@@ -70,15 +75,17 @@
             this.splitContainer1.Panel2.Controls.Add(this.label3);
             this.splitContainer1.Panel2.Controls.Add(this.averagingNumericUpDown);
             this.splitContainer1.Panel2.Controls.Add(this.label2);
-            this.splitContainer1.Size = new System.Drawing.Size(1077, 658);
-            this.splitContainer1.SplitterDistance = 449;
+            this.splitContainer1.Size = new System.Drawing.Size(1436, 810);
+            this.splitContainer1.SplitterDistance = 552;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // saveExposureSettingsButton
             // 
-            this.saveExposureSettingsButton.Location = new System.Drawing.Point(15, 161);
+            this.saveExposureSettingsButton.Location = new System.Drawing.Point(20, 198);
+            this.saveExposureSettingsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.saveExposureSettingsButton.Name = "saveExposureSettingsButton";
-            this.saveExposureSettingsButton.Size = new System.Drawing.Size(149, 23);
+            this.saveExposureSettingsButton.Size = new System.Drawing.Size(199, 28);
             this.saveExposureSettingsButton.TabIndex = 16;
             this.saveExposureSettingsButton.Text = "Save Exposure Settings";
             this.saveExposureSettingsButton.UseVisualStyleBackColor = true;
@@ -86,9 +93,10 @@
             // 
             // whiteRefButton
             // 
-            this.whiteRefButton.Location = new System.Drawing.Point(181, 42);
+            this.whiteRefButton.Location = new System.Drawing.Point(241, 52);
+            this.whiteRefButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.whiteRefButton.Name = "whiteRefButton";
-            this.whiteRefButton.Size = new System.Drawing.Size(113, 23);
+            this.whiteRefButton.Size = new System.Drawing.Size(151, 28);
             this.whiteRefButton.TabIndex = 13;
             this.whiteRefButton.Text = "Collect White Ref";
             this.whiteRefButton.UseVisualStyleBackColor = true;
@@ -96,9 +104,10 @@
             // 
             // darkRefButton
             // 
-            this.darkRefButton.Location = new System.Drawing.Point(181, 12);
+            this.darkRefButton.Location = new System.Drawing.Point(241, 15);
+            this.darkRefButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.darkRefButton.Name = "darkRefButton";
-            this.darkRefButton.Size = new System.Drawing.Size(113, 23);
+            this.darkRefButton.Size = new System.Drawing.Size(151, 28);
             this.darkRefButton.TabIndex = 12;
             this.darkRefButton.Text = "Collect Dark Ref";
             this.darkRefButton.UseVisualStyleBackColor = true;
@@ -107,10 +116,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 104);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(17, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(97, 16);
             this.label1.TabIndex = 10;
             this.label1.Text = "Dwell time [ms]";
             // 
@@ -121,15 +129,15 @@
             0,
             0,
             0});
-            this.dwellTimeNumericUpDown.Location = new System.Drawing.Point(97, 103);
-            this.dwellTimeNumericUpDown.Margin = new System.Windows.Forms.Padding(2);
+            this.dwellTimeNumericUpDown.Location = new System.Drawing.Point(129, 127);
+            this.dwellTimeNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dwellTimeNumericUpDown.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.dwellTimeNumericUpDown.Name = "dwellTimeNumericUpDown";
-            this.dwellTimeNumericUpDown.Size = new System.Drawing.Size(63, 20);
+            this.dwellTimeNumericUpDown.Size = new System.Drawing.Size(84, 22);
             this.dwellTimeNumericUpDown.TabIndex = 11;
             this.dwellTimeNumericUpDown.Value = new decimal(new int[] {
             10,
@@ -140,10 +148,10 @@
             // 
             // singleSpectrumButton
             // 
-            this.singleSpectrumButton.Location = new System.Drawing.Point(15, 12);
-            this.singleSpectrumButton.Margin = new System.Windows.Forms.Padding(2);
+            this.singleSpectrumButton.Location = new System.Drawing.Point(20, 15);
+            this.singleSpectrumButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.singleSpectrumButton.Name = "singleSpectrumButton";
-            this.singleSpectrumButton.Size = new System.Drawing.Size(63, 25);
+            this.singleSpectrumButton.Size = new System.Drawing.Size(84, 31);
             this.singleSpectrumButton.TabIndex = 7;
             this.singleSpectrumButton.Text = "Single";
             this.singleSpectrumButton.UseVisualStyleBackColor = true;
@@ -152,10 +160,10 @@
             // normalizedCheckBox
             // 
             this.normalizedCheckBox.AutoSize = true;
-            this.normalizedCheckBox.Location = new System.Drawing.Point(42, 133);
-            this.normalizedCheckBox.Margin = new System.Windows.Forms.Padding(2);
+            this.normalizedCheckBox.Location = new System.Drawing.Point(56, 164);
+            this.normalizedCheckBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.normalizedCheckBox.Name = "normalizedCheckBox";
-            this.normalizedCheckBox.Size = new System.Drawing.Size(118, 17);
+            this.normalizedCheckBox.Size = new System.Drawing.Size(148, 20);
             this.normalizedCheckBox.TabIndex = 9;
             this.normalizedCheckBox.Text = "Normalize spectrum";
             this.normalizedCheckBox.UseVisualStyleBackColor = true;
@@ -168,8 +176,8 @@
             0,
             0,
             0});
-            this.integrationTimeMsNumericUpDown.Location = new System.Drawing.Point(114, 76);
-            this.integrationTimeMsNumericUpDown.Margin = new System.Windows.Forms.Padding(2);
+            this.integrationTimeMsNumericUpDown.Location = new System.Drawing.Point(152, 94);
+            this.integrationTimeMsNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.integrationTimeMsNumericUpDown.Maximum = new decimal(new int[] {
             500,
             0,
@@ -181,7 +189,7 @@
             0,
             0});
             this.integrationTimeMsNumericUpDown.Name = "integrationTimeMsNumericUpDown";
-            this.integrationTimeMsNumericUpDown.Size = new System.Drawing.Size(47, 20);
+            this.integrationTimeMsNumericUpDown.Size = new System.Drawing.Size(63, 22);
             this.integrationTimeMsNumericUpDown.TabIndex = 6;
             this.integrationTimeMsNumericUpDown.Value = new decimal(new int[] {
             10,
@@ -192,10 +200,10 @@
             // 
             // ContinuousButton
             // 
-            this.ContinuousButton.Location = new System.Drawing.Point(82, 12);
-            this.ContinuousButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ContinuousButton.Location = new System.Drawing.Point(109, 15);
+            this.ContinuousButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ContinuousButton.Name = "ContinuousButton";
-            this.ContinuousButton.Size = new System.Drawing.Size(79, 25);
+            this.ContinuousButton.Size = new System.Drawing.Size(105, 31);
             this.ContinuousButton.TabIndex = 8;
             this.ContinuousButton.Text = "Continuous";
             this.ContinuousButton.UseVisualStyleBackColor = true;
@@ -204,24 +212,23 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(14, 51);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(19, 63);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.Size = new System.Drawing.Size(69, 16);
             this.label3.TabIndex = 4;
             this.label3.Text = "Averaging";
             // 
             // averagingNumericUpDown
             // 
-            this.averagingNumericUpDown.Location = new System.Drawing.Point(114, 50);
-            this.averagingNumericUpDown.Margin = new System.Windows.Forms.Padding(2);
+            this.averagingNumericUpDown.Location = new System.Drawing.Point(152, 62);
+            this.averagingNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.averagingNumericUpDown.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.averagingNumericUpDown.Name = "averagingNumericUpDown";
-            this.averagingNumericUpDown.Size = new System.Drawing.Size(47, 20);
+            this.averagingNumericUpDown.Size = new System.Drawing.Size(63, 22);
             this.averagingNumericUpDown.TabIndex = 3;
             this.averagingNumericUpDown.Value = new decimal(new int[] {
             1,
@@ -233,20 +240,39 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 77);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(19, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.Size = new System.Drawing.Size(126, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Integration time [ms]";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.spectrumProcessorChainControl1);
+            this.groupBox1.Location = new System.Drawing.Point(421, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(590, 246);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Spectrum Processor Chain";
+            // 
+            // spectrumProcessorChainControl1
+            // 
+            this.spectrumProcessorChainControl1.Location = new System.Drawing.Point(16, 25);
+            this.spectrumProcessorChainControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.spectrumProcessorChainControl1.Name = "spectrumProcessorChainControl1";
+            this.spectrumProcessorChainControl1.Quiet = false;
+            this.spectrumProcessorChainControl1.Size = new System.Drawing.Size(567, 214);
+            this.spectrumProcessorChainControl1.TabIndex = 17;
+            // 
             // SpectrometerControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "SpectrometerControl";
-            this.Size = new System.Drawing.Size(1077, 658);
+            this.Size = new System.Drawing.Size(1436, 810);
             this.Load += new System.EventHandler(this.SpectrometerDialog_Load);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
@@ -255,6 +281,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dwellTimeNumericUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.integrationTimeMsNumericUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.averagingNumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -274,5 +301,7 @@
         private System.Windows.Forms.Button whiteRefButton;
         private System.Windows.Forms.Button darkRefButton;
         private System.Windows.Forms.Button saveExposureSettingsButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private SpectrumProcessorChainControl spectrumProcessorChainControl1;
     }
 }
