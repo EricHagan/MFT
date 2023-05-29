4ed9cede28ae45cc1a8b6f125d26a91f84b2abdf
Matt Garrett <matt.garrett@ansys.com>
2023-05-28 21:42:05 -0600

    add scrollbars to SpectrumProcessorChainControl

--- MFT/SpectrumProcessorChainControl.Designer.cs
+++ MFT/SpectrumProcessorChainControl.Designer.cs
@@ -37,19 +37,20 @@
             // 
             // chainFlowLayoutPanel
             // 
-            this.chainFlowLayoutPanel.Location = new System.Drawing.Point(4, 53);
-            this.chainFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.chainFlowLayoutPanel.AutoScroll = true;
+            this.chainFlowLayoutPanel.Location = new System.Drawing.Point(4, 66);
+            this.chainFlowLayoutPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.chainFlowLayoutPanel.Name = "chainFlowLayoutPanel";
-            this.chainFlowLayoutPanel.Size = new System.Drawing.Size(359, 516);
+            this.chainFlowLayoutPanel.Size = new System.Drawing.Size(404, 645);
             this.chainFlowLayoutPanel.TabIndex = 0;
             this.chainFlowLayoutPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.chainFlowLayoutPanel_Paint);
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(404, 177);
-            this.addButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addButton.Location = new System.Drawing.Point(454, 221);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(100, 28);
+            this.addButton.Size = new System.Drawing.Size(112, 35);
             this.addButton.TabIndex = 1;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -58,42 +59,43 @@
             // availableProcessorsListBox
             // 
             this.availableProcessorsListBox.FormattingEnabled = true;
-            this.availableProcessorsListBox.ItemHeight = 16;
-            this.availableProcessorsListBox.Location = new System.Drawing.Point(371, 53);
-            this.availableProcessorsListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.availableProcessorsListBox.ItemHeight = 20;
+            this.availableProcessorsListBox.Location = new System.Drawing.Point(417, 66);
+            this.availableProcessorsListBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.availableProcessorsListBox.Name = "availableProcessorsListBox";
-            this.availableProcessorsListBox.Size = new System.Drawing.Size(159, 116);
+            this.availableProcessorsListBox.Size = new System.Drawing.Size(178, 144);
             this.availableProcessorsListBox.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Location = new System.Drawing.Point(20, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 3;
             this.label1.Text = "Name:";
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(71, 17);
+            this.nameTextBox.Location = new System.Drawing.Point(80, 21);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(459, 22);
+            this.nameTextBox.Size = new System.Drawing.Size(516, 26);
             this.nameTextBox.TabIndex = 4;
             this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
             // 
             // SpectrumProcessorChainControl
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.availableProcessorsListBox);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.chainFlowLayoutPanel);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "SpectrumProcessorChainControl";
-            this.Size = new System.Drawing.Size(547, 585);
+            this.Size = new System.Drawing.Size(615, 731);
             this.ResumeLayout(false);
             this.PerformLayout();
 
